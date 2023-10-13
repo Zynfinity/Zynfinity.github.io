e0d7892d8ba79d1898352fba9c396005b730ed53
Fajar Ihsana <fajarihsana15@gmail.com>
2023-10-13 17:49:47 +0700

    yo

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const switchMode = document.querySelector('.switch input[type="checkbox"]')
-const switchMode2 =  document.querySelector('.switch-responsive input[type="checkbox"]')
-function darkMode(s){
-    if(s.target.checked){
+const switchMode2 = document.querySelector('.switch-responsive input[type="checkbox"]')
+function darkMode(s) {
+    if (s.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
-    }else{
+    } else {
         document.documentElement.setAttribute('data-theme', 'light');
     }
 }
@@ -12,33 +12,33 @@ switchMode2.addEventListener('change', darkMode, false);
 
 const ul = document.querySelector('ul')
 const aa = document.querySelectorAll('a')
-ul.addEventListener('click', function(e){
-    if(e.target.classList.contains('menu')){
-        aa.forEach(function(a){
+ul.addEventListener('click', function (e) {
+    if (e.target.classList.contains('menu')) {
+        aa.forEach(function (a) {
             a.className = "menu"
         })
         e.target.classList.add('active')
     }
 })
-function playAudio(){
+function playAudio() {
     const audio = document.getElementById('myAudio')
     audio.play()
 }
 
 // FILL PROJECT HTML
-function getRepositories(){
+function getRepositories() {
     var ul = $('div.project-wrapper');
     var count = 15; // number of images
-    axios.get('https://api.github.com/users/Zynfinity/subscriptions').then(async ({data}) => {
+    axios.get('https://api.github.com/users/Zynfinity/subscriptions').then(async ({ data }) => {
         const filter = data.filter(s => s.language != null)
         data = filter
-        for(var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             ul.append(`<div class="list animate__animated animate__bounceInLeft" id=${i}></div>`);
             const div = $(`div > #${i}`)
             //img and title
             div.append(`<div class="repo-title" id=title-${i}></div>`)
             const title = $(`div > #title-${i}`)
-            title.append(`<img class="github-picture" src=${data[i].owner.avatar_url}/>`)
+            // title.append(`<img class="github-picture" src=${data[i].owner.avatar_url}/>`)
             title.append(`<h3>${data[i].name}</h3>`)
             //language
             div.append(`<span class="language">${data[i].language ? data[i].language : ''}</span>`)
