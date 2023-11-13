0662a7c531847cc72aef7f3c6aab68434800304d
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2023-11-13 21:04:26 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>11220131 - Fajar Ihsana - Latihan If</title>
+</head>
+
+<body>
+    <?php
+    $nama = "Fajar";
+    $Nama = "Fajar Ihsana";
+    echo "Isi variable \$nama = $nama";
+    echo "<br>";
+    echo "Isi variable \$Nama = $Nama";
+    ?>
+</body>
+
+</html>
